272327fee904be3c456ebe7cb1e163c7dc3bd5f5
Skyler W Lee <skylerlee4950@gmail.com>
2022-03-10 15:28:21 -0800

    GUI work BEFORE designer revamp

--- Gui.py
+++ Gui.py
@@ -1,12 +1,14 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5 import uic
-from PyQt5.QtWidgets import QDialog, QApplication
+from PyQt5.QtWidgets import QDialog, QApplication, QDesktopWidget, QGraphicsScene, QGraphicsItem, QGraphicsView, QHBoxLayout
+from PyQt5.QtGui import QFont, QColor, QPalette, QPixmap
+from PyQt5.QtCore import Qt
 
 baseUIClass, baseUIWidget = uic.loadUiType("chess.ui")
 class Logic(baseUIWidget, baseUIClass):
-    def __init__(self, parent = None):
-        super(Logic, self).__init__(parent)
+    def __init__(self):
+        super().__init__()
         self.setupUi(self)
         self.tableWidget.setColumnWidth(0, 50)
         self.tableWidget.setColumnWidth(1, 150)
@@ -25,6 +27,20 @@ class Logic(baseUIWidget, baseUIClass):
         self.pushButton_6,
         ]
 
+        self.resize(1500, 1000)
+        self.center()
+
+        self.board()
+    
+    def board(self):
+        self.scene = QGraphicsScene(0, 0, 100, 100)
+        pixmap = QPixmap('assets/images/simple/Board.png')
+        self.scene.addPixmap(pixmap)
+        view = QGraphicsView(self.scene)
+        hbox = QHBoxLayout(self)
+        hbox.addWidget(view)
+        self.setLayout(hbox)
+
     def load_data(self):
         log = [{'move': 1, 'white': 'e2e4', 'black': 'e7e5'}]
         row = 0
@@ -38,11 +54,46 @@ class Logic(baseUIWidget, baseUIClass):
     def setup_board(self):
         self.label.setPixmap(QtGui.QPixmap('assets/images/simple/Board.png'))
 
+    def center(self):
+        rect = self.frameGeometry()
+        user = QDesktopWidget().availableGeometry().center()
+        rect.moveCenter(user)
+        self.move(rect.topLeft())
+
+def dark_palette():
+    # Returns a QPalette object with a dark style! 
+    # Shamelessly stolen
+
+    dark_palette = QPalette()
+
+    dark_palette.setColor(QPalette.Window, QColor(53, 53, 53))
+    dark_palette.setColor(QPalette.WindowText, Qt.white)
+    dark_palette.setColor(QPalette.Base, QColor(25, 25, 25))
+    dark_palette.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
+    dark_palette.setColor(QPalette.ToolTipBase, Qt.white)
+    dark_palette.setColor(QPalette.ToolTipText, Qt.white)
+    dark_palette.setColor(QPalette.Text, Qt.white)
+    dark_palette.setColor(QPalette.Button, QColor(53, 53, 53))
+    dark_palette.setColor(QPalette.ButtonText, Qt.white)
+    dark_palette.setColor(QPalette.BrightText, Qt.red)
+    dark_palette.setColor(QPalette.Link, QColor(42, 130, 218))
+    dark_palette.setColor(QPalette.Highlight, QColor(42, 130, 218))
+    dark_palette.setColor(QPalette.HighlightedText, Qt.black)
+
+    return dark_palette
+
 def main():
     app = QtWidgets.QApplication(sys.argv)
+
+    app.setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }")
+    app.setStyle('Fusion')
+    app.setPalette(dark_palette())
+
     ui = Logic()
-    #ui.showMaximized()
     ui.show()
+    #ui.showMaximized()
+
     sys.exit(app.exec_())
 
-main()
+if __name__ == '__main__':
+    main()
